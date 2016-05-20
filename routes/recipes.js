788368f0c22c1b2de74969488d146f30016b7f48
Jesper Smith <Jesper Smith>
2016-05-20 11:32:25 +0200

    wee 14

--- routes/recipes.js
+++ routes/recipes.js
@@ -120,10 +120,14 @@ app.put('/recipes/:id', function(req, res) {
         var collection = db.collection('recipes');
 
         collection.update({ '_id': ObjectId(req.params.id) }, {
-            $set: req.body
-            
+            //$set: req.body 
+            'name': req.body.name,
+            'cuisine': req.body.cuisine,
+            'category': req.body.category,
+            'time': req.body.time,
+            'description': req.body.description,
+            'link': req.body.link
         }, function(err, data) {
-             
             res.status(200);
             //res.send({ 'msg': ''+ req.body});
             console.dir(req.body);
